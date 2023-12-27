9cf5a5e996333d7832d8be9c2f0bf9fb7e60acdc
Jenna Moon <144712813+stardust09301@users.noreply.github.com>
2023-12-26 20:59:38 -0500

    MyFirstApp_1 update

---
+++ MyFirstApp_1/MyFirstApp.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // input output stream
+using namespace std;// std is global level
+
+int main()
+{
+    //without 'using namespace std;' you hve to write std::
+    //std::cout << "Hello World!\n"; //this will output to the console hello world. std is namespace for cout
+    //std::cout << "My name is Jenna."<<std::endl;
+    //std::cout << "Today is Sunday.";
+    cout << "Hello World!\n"; //this will output to the console hello world. std is namespace for cout
+    cout << "My name is Jenna." << endl;
+    cout << "Today is Sunday.";
+    cout << "Have a nice day.";
+
+    system("pause>0"); //will stop other texts
+}
+
