508b4e076451ea0801f8be8108d0405740745a4d
Jenna Moon <144712813+stardust09301@users.noreply.github.com>
2024-01-12 22:36:29 -0500

    MultiDynamicArray update

---
+++ MultiDynamicArray/MultiDynamicArray.cpp
@@ -0,0 +1,30 @@
+// MultiDynamicArray.cpp =>Array of arrays
+// Create two-dimensional array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+    cout << "rows, cols:";
+    cin >> rows >> cols;
+
+    int** table = new int*[rows];
+    for (int i = 0; i < rows; i++) {
+        table[i] = new int[cols];
+    }
+
+    //table[1][2] = 88;
+
+    //delete corresponding array
+    for (int i = 0; i < rows; i++) {
+        delete[] table[i];
+    }
+    delete[] table;
+    table = NULL;
+
+    system("pause>0");
+    return 0;
+}
+
